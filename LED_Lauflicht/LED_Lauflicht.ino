735d4cb3256994a00fa5f798c42c8eae66863c72
Kl0ska <71316060+Kl0ska@users.noreply.github.com>
2020-09-20 21:35:50 +0200

    Array Lauflicht

--- LED_Lauflicht/LED_Lauflicht.ino
+++ LED_Lauflicht/LED_Lauflicht.ino
@@ -2,32 +2,31 @@
 
 /* Arduino Lauflicht
    Sven Kloska
-   11.09.2020
+   17.09.2020
    Arduino Uno (Chinakopie)
-   Grüne LED an Pin 3
+   LEDs an Pins 4, 5, 6, 7
 */
 
 
-#define LED_1 4 //Variablen für die Ausgangspins der LEDs
-#define LED_2 5
-#define LED_3 6
-#define LED_4 7
+byte led[4] = {4, 5, 6, 7};
 
-#define Takt 1000  //Der Takt für die LEDs in Millisekunden
+#define Takt 10000  //Der Takt für die LEDs in Millisekunden
 
-int counter = 0;
+int counter = 0;  //Zählvariable für die LED
 
 unsigned long time = millis();  //Variable um die Zeit zu speichern für das Blinken
 
-bool GRN_Status = false;  //Variable für den Status, den die LED haben soll
+
+
 
 void setup() {
 
   //Pins zuweisen für die LEDs
-  pinMode(LED_1, OUTPUT);
-  pinMode(LED_2, OUTPUT);
-  pinMode(LED_3, OUTPUT);
-  pinMode(LED_4, OUTPUT);
+
+  for (byte i = 0; i < 4; i++)
+  {
+    pinMode(led[i], OUTPUT);
+  }
 
   //Initialisiere die serielle Kommunikation
   Serial.begin(9600);
@@ -37,55 +36,71 @@ void setup() {
 
 void loop() {
 
+  /*
+    if (millis() - time > Takt)  //Millis gibt die Laufzeit wieder, ist diese abzüglich time größer 1000 wird die Bedingung ausgeführt
+    {
+      counter = counter + 1;  //Zähle die counter-Variable hoch um die nächste LED anzusteuern
+      time = millis(); //Schreibe die aktuelle Zeit in time, damit Laufzeit minus time 0 ergibt
+    }
+    if (counter == 5) //wenn der counter außerhalb des Bereichs ist wird dieser auf 1 zurückgesetzt
+    {
+      counter = 1;
+    }
+  */
+  int i = 0;
 
-  if (millis() - time > Takt)  //Millis gibt die Laufzeit wieder, ist diese abzüglich time größer 1000 wird die Bedingung ausgeführt
-  {
-    counter = counter + 1;  //Zähle die counter-Variable hoch um die nächste LED anzusteuern
-    time = millis(); //Schreibe die aktuelle Zeit in time, damit Laufzeit minus time 0 ergibt
-  }
-  if (counter == 5) //wenn der counter außerhalb des Bereichs ist wird dieser auf 1 zurückgesetzt
-  {
-    counter = 1;
-  }
 
-
-LEDs:
-  //Wenn die Variable i den Wert für die LED hat wird der Ausgang der LED eingeschaltet, sonst aus
-  if (counter == 1)
+  for (i = 0; i < 4; i++)
   {
-    digitalWrite(LED_1, HIGH);
+    digitalWrite(led[i], HIGH);
+    delay(500);
   }
-  else
+  for (i = 4; i >= 0; i--)
   {
-    digitalWrite(LED_1, LOW);
-  }
-  if (counter == 2)
-  {
-    digitalWrite(LED_2, HIGH);
-  }
-  else
-  {
-    digitalWrite(LED_2, LOW);
-  }
-  if (counter == 3)
-  {
-    digitalWrite(LED_3, HIGH);
-  }
-  else
-  {
-    digitalWrite(LED_3, LOW);
-  }
-  if (counter == 4)
-  {
-    digitalWrite(LED_4, HIGH);
-  }
-  else
-  {
-    digitalWrite(LED_4, LOW);
+    digitalWrite(led[i], LOW);
+    delay(500);
   }
 
 
 
+  /*
+    LEDs:
+    //Wenn die Variable counter den Wert für die LED hat wird der Ausgang der LED eingeschaltet, sonst aus
+    if (counter == 1)
+    {
+      digitalWrite(LED_1, HIGH);
+    }
+    else
+    {
+      digitalWrite(LED_1, LOW);
+    }
+    if (counter == 2)
+    {
+      digitalWrite(LED_2, HIGH);
+    }
+    else
+    {
+      digitalWrite(LED_2, LOW);
+    }
+    if (counter == 3)
+    {
+      digitalWrite(LED_3, HIGH);
+    }
+    else
+    {
+      digitalWrite(LED_3, LOW);
+    }
+    if (counter == 4)
+    {
+      digitalWrite(LED_4, HIGH);
+    }
+    else
+    {
+      digitalWrite(LED_4, LOW);
+    }
+
+
+  */
 
   //Debugging Ausgabe:
   Serial.print ("Variable ");
