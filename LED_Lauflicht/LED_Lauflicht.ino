ba0abe906de9f9398dfd754cbe6ca4758fcfb575
Kl0ska <71316060+Kl0ska@users.noreply.github.com>
2020-09-22 14:09:50 +0200

    LED 2D Matrix

--- LED_Lauflicht/LED_Lauflicht.ino
+++ LED_Lauflicht/LED_Lauflicht.ino
@@ -8,9 +8,22 @@
 */
 
 
-byte led[4] = {4, 5, 6, 7};
+byte led[8] = {2, 3, 4, 5, 6, 7, 8, 9};
 
-#define Takt 10000  //Der Takt für die LEDs in Millisekunden
+byte ledZustand[10][8] = {
+  {1, 0, 0, 0, 0, 0, 0, 1},
+  {0, 1, 0, 0, 0, 0, 1, 0},
+  {0, 0, 1, 0, 0, 1, 0, 0},
+  {0, 0, 0, 1, 1, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 1, 1, 0, 0, 0},
+  {0, 0, 1, 0, 0, 1, 0, 0},
+  {0, 1, 0, 0, 0, 0, 1, 0},
+  {1, 0, 0, 0, 0, 0, 0, 1},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+};
+
+int takt = 0;  //Der Takt für die LEDs in Millisekunden
 
 int counter = 0;  //Zählvariable für die LED
 
@@ -23,7 +36,7 @@ void setup() {
 
   //Pins zuweisen für die LEDs
 
-  for (byte i = 0; i < 4; i++)
+  for (byte i = 0; i < 8; i++)
   {
     pinMode(led[i], OUTPUT);
   }
@@ -36,79 +49,19 @@ void setup() {
 
 void loop() {
 
-  /*
-    if (millis() - time > Takt)  //Millis gibt die Laufzeit wieder, ist diese abzüglich time größer 1000 wird die Bedingung ausgeführt
-    {
-      counter = counter + 1;  //Zähle die counter-Variable hoch um die nächste LED anzusteuern
-      time = millis(); //Schreibe die aktuelle Zeit in time, damit Laufzeit minus time 0 ergibt
-    }
-    if (counter == 5) //wenn der counter außerhalb des Bereichs ist wird dieser auf 1 zurückgesetzt
-    {
-      counter = 1;
-    }
-  */
-  int i = 0;
+
+  takt = 150;
 
 
-  for (i = 0; i < 4; i++)
+  for (byte i = 0; i < 10; i++)  //Zeilen
   {
-    digitalWrite(led[i], HIGH);
-    delay(500);
+    for (byte j = 0; j < 8; j++)  //Spalten
+    {
+      digitalWrite(led[j], ledZustand[i][j]);
+
+    }
+    delay(takt);
   }
-  for (i = 4; i >= 0; i--)
-  {
-    digitalWrite(led[i], LOW);
-    delay(500);
-  }
-
-
-
-  /*
-    LEDs:
-    //Wenn die Variable counter den Wert für die LED hat wird der Ausgang der LED eingeschaltet, sonst aus
-    if (counter == 1)
-    {
-      digitalWrite(LED_1, HIGH);
-    }
-    else
-    {
-      digitalWrite(LED_1, LOW);
-    }
-    if (counter == 2)
-    {
-      digitalWrite(LED_2, HIGH);
-    }
-    else
-    {
-      digitalWrite(LED_2, LOW);
-    }
-    if (counter == 3)
-    {
-      digitalWrite(LED_3, HIGH);
-    }
-    else
-    {
-      digitalWrite(LED_3, LOW);
-    }
-    if (counter == 4)
-    {
-      digitalWrite(LED_4, HIGH);
-    }
-    else
-    {
-      digitalWrite(LED_4, LOW);
-    }
-
-
-  */
-
-  //Debugging Ausgabe:
-  Serial.print ("Variable ");
-  Serial.print (counter);
-  Serial.print (" Time ");
-  Serial.println (time);
-
-  //Serial.println (millis);
 
 
 }
